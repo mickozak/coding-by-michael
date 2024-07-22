27d15efbc72211bbf8a762553e9e09f769d00eb8
Michał Kozak <kozak_m@outlook.com>
2024-07-22 17:57:36 +0200

    Create User.js

---
+++ 1/User.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_USERS = gql`
+  query GetUsers {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
+
+const Users = () => {
+  const { loading, error, data } = useQuery(GET_USERS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <ul>
+      {data.users.map(user => (
+        <li key={user.id}>
+          {user.name} - {user.email}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Users;
