6e480ba382b92c7a27afa4bde27415548f8adaf5
Michał Kozak <kozak_m@outlook.com>
2024-07-26 16:32:34 +0200

    Create index.js

---
+++ 2/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client';
+import App from './App';
+
+const client = new ApolloClient({
+  uri: 'https://your-graphql-endpoint.com/graphql',
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
